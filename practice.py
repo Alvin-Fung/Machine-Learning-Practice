6fc445fa221e5dea91cb4382d5e3358a729c82f7
Alvin-Fung <alvin_fung9@outlook.com>
2024-09-11 15:34:42 +0100

    Scatter plot

--- practice.py
+++ practice.py
@@ -20,3 +20,19 @@ x = np.random.uniform(0.0, 5.0, 100000) # Low, High, Size
 y = np.random.normal(5.0, 1.0, 100000) # Produces a bell curve in the graph - Mean value, standard deviation and size
 plt.hist(y, 100)
 plt.show()
+
+# Scatter Plot
+
+x = [5, 7, 8, 7, 12, 2, 4, 17, 18, 5, 6]
+y = [52, 99, 87, 76, 54, 32, 101,87, 94, 78, 121]
+
+plt.scatter(x,y)
+plt.show()
+
+# Scatter plot with random data distribution
+
+x = np.random.normal(5.0, 1.0, 1000)
+y = np.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x, y)
+plt.show()
