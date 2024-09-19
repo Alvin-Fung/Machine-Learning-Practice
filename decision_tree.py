74839131c0ca42c2ca1049a405c0cd124966b71d
Alvin-Fung <alvin_fung9@outlook.com>
2024-09-19 17:05:33 +0100

    Creating a decision tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,12 +1,30 @@
 import pandas as pd
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
 
+
+df = pd.read_csv("data2.csv")
 
 d = {'UK': 0, 'USA': 1, 'N': 2}
-df = pd.read_csv("data2.csv")
-d = {'YES': 1, 'NO': 0}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'Yes': 1, 'No': 0}
 df['Go'] = df['Go'].map(d)
 
-feat
 
+# Impute missing values in 'Go' column with the mean
+df['Go'] = df['Go'].fillna(df['Go'].mean())  # Replace NaN with mean
 
-print(df)
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+X = df[features]
+y = df['Go']
+
+# Train the decision tree
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+plot_tree(dtree, feature_names=features)
+plt.show()
+
+# This doesn't work... why..?
