a9ea5b88bc43d9b194b5076a42511fc004c72d29
Alvin Fung <alvin_fung9@outlook.com>
2024-10-31 14:45:13 +0000

    Data frame filtering

--- student_prediction.py
+++ student_prediction.py
@@ -1,3 +1,5 @@
+# Task: Predict a student's grades based on the attributes presented in the data set.
+
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
@@ -7,4 +9,8 @@ from scipy import stats
 
 data = pd.read_csv("student_mat.csv", sep=";")
 # Data is being separated by semicolons
+print(data.head())
+
+# Data Frame Filtering - we need to specifically filter the data set to predict their grades.
+data = data[["absences", "studytime", "failures", "G1", "G2", "G3"]]
 print(data.head())
