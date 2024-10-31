53757b287a47e44485088e6780c43211cd44f228
Alvin Fung <alvin_fung9@outlook.com>
2024-10-31 14:23:31 +0000

    CSV rename and loading data

--- student_prediction.py
+++ student_prediction.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
-import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 from scipy import stats
+
+data = pd.read_csv("student_mat.csv", sep=";")
+# Data is being separated by semicolons
+print(data.head())
