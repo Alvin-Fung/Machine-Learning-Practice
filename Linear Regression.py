73fd6bd57c29b882b77d4a72006666a7baae1a84
Alvin Fung <alvin_fung9@outlook.com>
2024-11-11 14:55:09 +0000

    Training the model for the best score

--- Linear Regression.py
+++ Linear Regression.py
@@ -22,17 +22,21 @@ predict = "G3"
 X = np.array(data.drop([predict], axis=1))  # New training data using G3
 Y = np.array(data[predict])  # All of our labels
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-    X, Y, test_size=0.1)  # Splits data up into 10% for test samples
+best_score = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, Y, test_size=0.1)  # Splits data up into 10% for test samples
 
-"""linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)  # Line of best fit
-acc = linear.score(x_test, y_test)  # Represents accurracy of the model
-print(acc)
+    linear.fit(x_train, y_train)  # Line of best fit
+    acc = linear.score(x_test, y_test)  # Represents accurracy of the model
+    print(acc)
 
-with open("studentmodel.pickle", "wb") as f:
-    pickle.dump(linear, f)"""
+    if acc > best_score:
+        best_score = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
 
 pickle_in = open("studentmodel.pickle", "rb")
 # Loading the pickle into the linear model
