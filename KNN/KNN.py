e16b799a43614424add1ed04d80c8794612f0095
Alvin Fung <alvin_fung9@outlook.com>
2024-11-12 12:34:12 +0000

    rest of the fit_transform()s and splitting the data for training and testing

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -10,3 +10,18 @@ print(data.head())
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+# print(buying)
+
+predict = "class"
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    X, y, test_size=0.1)  # Splits data up into 10% for test samples
